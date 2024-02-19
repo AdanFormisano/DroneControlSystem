9ba7da512c0c4919d8f23945643efe84f28c7dcc
AdanFormisano <formisano.1813917@studenti.uniroma1.it>
2024-02-19 16:19:53 +0100

    Implemented tests for drone and managing data

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,23 @@
 #include <spdlog/spdlog.h>
 #include "Drone.h"
 
+using namespace sw::redis;
+
 int main() {
     // Start DroneControl
     DroneControl droneControl;
 
     // For testing purposes create a single drone
     spdlog::info("Creating a test drone");
-    drones::Drone testDrone;
+    drones::Drone testDrone(1);
     spdlog::info("Test drone {} created", testDrone.getId());
+    
+    // Get drone data from Redis
+    DroneData data = droneControl.getDroneData(testDrone.getId());
+    spdlog::info("Drone {} status: {}", data.id, data.status);
+
+    // Upload data from drone to Redis
+    testDrone.uploadData();
 
     // Start ChargingBase
 
@@ -17,4 +26,3 @@ int main() {
 
     return 0;
 }
-
