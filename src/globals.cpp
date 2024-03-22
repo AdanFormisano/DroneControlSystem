a8953ac48b85397d49b8e825ee6efc88a8dd406e
Mark <83904681+Mark3xtrm@users.noreply.github.com>
2024-03-22 17:37:38 +0100

    Aggiornate le impostazioni di simulazione

--- src/globals.cpp
+++ src/globals.cpp
@@ -5,5 +5,5 @@ std::string sync_counter_key = "sync_process_count";
 std::string sync_channel = "SYNC";
 
 // Simulation settings CHANGE THIS FIRST IF PC IS MELTING!!!!
-std::chrono::milliseconds tick_duration_ms = std::chrono::milliseconds(50);     // duration of 1 tick in milliseconds. 50ms is minimum I could set without the simulation breaking
-std::chrono::milliseconds sim_duration_ms = std::chrono::milliseconds(500000);  // duration of the simulation in milliseconds
+std::chrono::milliseconds tick_duration_ms = std::chrono::milliseconds(100);    // duration of 1 tick in milliseconds. 50ms is minimum I could set without the simulation breaking
+std::chrono::milliseconds sim_duration_ms = std::chrono::milliseconds(1000000); // duration of the simulation in milliseconds
