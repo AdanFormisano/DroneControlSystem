716d60b9b1715eb2bf6c1dc1df3982313889b767
AdanFormisano <formisano.1813917@studenti.uniroma1.it>
2024-08-08 18:50:32 +0200

    Cleanup of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <spdlog/spdlog.h>
 #include <sw/redis++/redis++.h>
 #include <unistd.h>
-
 #include <iostream>
 
 #include "../utils/RedisUtils.h"
@@ -14,80 +13,93 @@
 
 using namespace sw::redis;
 
-/* The simulation starts after all the process have initiated and are sync to one another.
- * The tick duration is 1 second, and it simulates 2.42 real time seconds (time needed for a drone to move to the next
- * "checkpoint") in 1 tick.
- * */
+/* The simulation is diveded in 4 processes: Main, DroneControl, Drone, ChargeBase, TestGenerator.
+ *  - Main: is the parent process, and creates the other 4 processes.
+ *  - DroneControl: manages the system and always has knowledge of the drones' state, it also sends commands to the drones.
+ *  - Drone: manages the simulation of the drones, and sends their state to the DroneControl.
+ *  - ChargeBase: manages the charging slots and assigns drones to them, manages the charging process.
+ *  - TestGenerator: generates test data for the simulation, simulating every possible outcome.
+ *
+ * The simulation starts after all the processes have initiated and are sync to one another.
+ * The tick duration is 1 second (can be modified in globals.cpp), and it simulates 2.42 real time seconds
+ * (time needed for a drone to move to the next "checkpoint") in 1 tick.
+ */
 
-// FIXME: All simulation should run from the main file
 int main() {
     spdlog::set_pattern("[%T.%e][%^%l%$][Main] %v");
 
+    // Create the DroneControl process
     pid_t pid_drone_control = fork();
     if (pid_drone_control == -1) {
         spdlog::error("Fork for DroneControl failed");
         return 1;
-    } else if (pid_drone_control == 0) {
-        // In child DroneControl process
-        auto drone_control_redis = Redis("tcp://127.0.0.1:7777");
-        //        drone_control_redis.incr(sync_counter_key);
-        utils::AddThisProcessToSyncCounter(drone_control_redis, "DroneControl");
+    } else if (pid_drone_control == 0) {    // In child DroneControl process
         spdlog::set_pattern("[%T.%e][%^%l%$][DroneControl] %v");
+
+        // Create the Redis object for DroneControl
+        auto drone_control_redis = Redis("tcp://127.0.0.1:7777");
+
+        // Sync of processes
+        utils::AddThisProcessToSyncCounter(drone_control_redis, "DroneControl");
 
         // Create the DroneControl object
         drone_control::DroneControl dc(drone_control_redis);
+
         // Start simulation
         dc.Run();
 
-    } else {
-        // In parent process create new child Drones process
+    } else {    // In parent process create new child Drones process
         pid_t pid_drone = fork();
         if (pid_drone == -1) {
             spdlog::error("Fork for Drone failed");
             return 1;
-        } else if (pid_drone == 0) {
-            // In child Drones process
+        } else if (pid_drone == 0) {    // In child Drones process
+            // Create the Redis object for Drone
             auto drone_redis = Redis("tcp://127.0.0.1:7777");
-            //            drone_redis.incr(sync_counter_key);
+
+            // Sync of processes
             utils::AddThisProcessToSyncCounter(drone_redis, "Drone");
+
             // Create the DroneManager object
             drones::DroneManager dm(drone_redis);
+
+            // Start simulation
             dm.Run();
 
-        } else {
-            // In parent process create new child ChargeBase process
+        } else {    // In parent process create new child ChargeBase process
             pid_t pid_charge_base = fork();
             if (pid_charge_base == -1) {
                 spdlog::error("Fork for ChargeBase failed");
                 return 1;
-            } else if (pid_charge_base == 0) {
-                // In child ChargeBase process
+            } else if (pid_charge_base == 0) {  // In child ChargeBase process
+                // Create the Redis object for ChargeBase
                 auto charge_base_redis = Redis("tcp://127.0.0.1:7777");
-                //                charge_base_redis.incr(sync_counter_key);
+
+                // Sync of processes
                 utils::AddThisProcessToSyncCounter(charge_base_redis, "ChargeBase");
+
                 // Create the ChargeBase object
                 auto cb = charge_base::ChargeBase::getInstance(charge_base_redis);
-                // cb->SetRedis(charge_base_redis);
 
                 // Set the charge rate for the charging slots
                 thread_local std::random_device rd;
-                cb->SetEngine(rd);
+                cb->SetEngine(rd);  // TODO: Check if every slot has the same charge rate
 
-                //                utils::SyncWait(charge_base_redis);
+                // Sync of processes
                 utils::NamedSyncWait(charge_base_redis, "ChargeBase");
 
                 // Start simulation
                 cb->Run();
 
-            } else {
-                // In parent process create new child TestGenerator process
+            } else {    // In parent process create new child TestGenerator process
                 pid_t pid_test_generator = fork();
                 if (pid_test_generator == -1) {
                     spdlog::error("Fork for TestGenerator failed");
                     return 1;
-                } else if (pid_test_generator == 0) {
-                    // In child TestGenerator process
+                } else if (pid_test_generator == 0) {   // In child TestGenerator process
                     spdlog::set_pattern("[%T.%e][%^%l%$][TestGenerator] %v");
+
+                    // Create the Redis object for TestGenerator
                     auto test_redis = Redis("tcp://127.0.0.1:7777");
 
                     // Create the TestGenerator object
@@ -97,14 +109,11 @@ int main() {
 
                     // Start test generation
                     tg.Run();
-                } else {
-                    // In Main process
+                } else {    // In Main process
                     auto main_redis = Redis("tcp://127.0.0.1:7777");
-                    //                main_redis.incr(sync_counter_key);
-                    utils::AddThisProcessToSyncCounter(main_redis, "Main");
 
-                    // Wait for the other processes to finish initialization
-                    //                utils::SyncWait(main_redis);
+                    // Sync of processes
+                    utils::AddThisProcessToSyncCounter(main_redis, "Main");
                     utils::NamedSyncWait(main_redis, "Main");
 
                     // Start monitors
