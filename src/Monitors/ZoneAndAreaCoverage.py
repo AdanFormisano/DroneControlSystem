be62512a18fe8b7c4dc6b8938a11bfa47b2f6f2b
AdanFormisano <formisano.1813917@studenti.uniroma1.it>
2024-08-12 20:12:06 +0200

    Removed deprecated python files

--- src/Monitors/ZoneAndAreaCoverage.py
+++
@@ -1,7 +0,0 @@
-"""
-- checkAreaCoverage(): This monitor checks that all the points of the 6x6 Km area are verified (covered by the drones)
-    every 5 minutes.
-- checkZoneVerification(): This monitor checks that each zone is verified every 5 minutes (this has some internal
-    implications).
-"""
-
