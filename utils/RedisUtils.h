71a2233fc5c105119d0811dfe55878febfd56644
Mark <83904681+Mark3xtrm@users.noreply.github.com>
2024-09-30 12:16:03 +0200

    Added some RedisUtils for Redis cache

--- utils/RedisUtils.h
+++ utils/RedisUtils.h
@@ -1,25 +1,30 @@
 #ifndef DRONECONTROLSYSTEM_REDISUTILS_H
 #define DRONECONTROLSYSTEM_REDISUTILS_H
 
-#include <sw/redis++/redis++.h>
 #include <chrono>
+#include <string>
+#include <sw/redis++/redis++.h>
 
 using namespace sw::redis;
 
 namespace utils {
-    int RedisConnectionCheck(sw::redis::Redis& redis, std::string clientName);
-    long long RedisGetClientID(sw::redis::Redis& redis);
-    bool getSimStatus(sw::redis::Redis& redis);
+int RedisConnectionCheck(sw::redis::Redis &redis, std::string clientName);
+long long RedisGetClientID(sw::redis::Redis &redis);
+bool getSimStatus(sw::redis::Redis &redis);
 
-    // Used for synchronization of processes
-    void AddThisProcessToSyncCounter(Redis& redis, const std::string& process_name);
-    int NamedSyncWait(Redis& redis, const std::string& process_name);
+// Clear Redis cache
+void clearRedis();
+void clearCache(const std::string &cachePath);
 
-    // Used for synchronization of Scanner and DroneControl processes
-    void UpdateSyncTick(Redis &redis, int tick_n);
-    void AckSyncTick(Redis &redis, int tick_n);
-    bool CheckSyncTick(Redis &redis, int tick_n);
-    int GetSyncTick(Redis &redis);
-}
+// Used for synchronization of processes
+void AddThisProcessToSyncCounter(Redis &redis, const std::string &process_name);
+int NamedSyncWait(Redis &redis, const std::string &process_name);
 
-#endif //DRONECONTROLSYSTEM_REDISUTILS_H
+// Used for synchronization of Scanner and DroneControl processes
+void UpdateSyncTick(Redis &redis, int tick_n);
+void AckSyncTick(Redis &redis, int tick_n);
+bool CheckSyncTick(Redis &redis, int tick_n);
+int GetSyncTick(Redis &redis);
+} // namespace utils
+
+#endif // DRONECONTROLSYSTEM_REDISUTILS_H
